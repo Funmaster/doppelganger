98a0c0863b237b6cecf0586ff795ccc2648eab86
edward@thezees.net <edward@thezees.net@d4dccdc8-60c2-11de-a55d-e76e375ae706>
2011-11-25 15:26:19 +0000

    Initial commit of python doppelganger

---
+++ doppelganger.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+
+import gzip
+
+import cStringIO
+
+from twisted.web import server, resource, http
+from twisted.web.proxy import Proxy, ProxyRequest, ProxyClient, ProxyClientFactory
+from twisted.internet import reactor
+		
+class DoppelProxyClient(ProxyClient):
+	def __init__(self, *args, **kwargs):
+		ProxyClient.__init__(self, *args, **kwargs)
+		self.buffer = ""
+		self.test = "<html><head><title>Doppelganger</title></head><body></body></html>"
+
+	def handleHeader(self, key, value):
+		'''print "[ '", key, "': ", value, "]"''' 
+		ProxyClient.handleHeader(self, key, value)
+		
+	def handleResponsePart(self, buffer):
+		self.buffer += buffer
+		ProxyClient.handleResponsePart(self, buffer)
+		
+	def handleResponseEnd(self):
+		
+		'''self.father.responseHeaders.setRawHeaders("content-length", [len(self.test)])
+	'''
+		
+		data = gzip.GzipFile(fileobj = cStringIO.StringIO(self.buffer)).read()
+		print data
+		
+		
+		ProxyClient.handleResponseEnd(self)
+		
+class DoppelProxyClientFactory(ProxyClientFactory):
+	protocol = DoppelProxyClient
+		
+class DoppelProxyRequest(ProxyRequest):
+	protocols = { 'http': DoppelProxyClientFactory }
+	ports = { 'http': 80 }
+	
+	def process(self):
+		'''self.requestHeaders.removeHeader('accept-encoding')'''
+		ProxyRequest.process(self)
+		
+class DoppelProxy(Proxy):
+	requestFactory = DoppelProxyRequest
+
+doppelProxy = http.HTTPFactory()
+doppelProxy.protocol = DoppelProxy
+
+		
+reactor.listenTCP(8000, doppelProxy)
+reactor.run()
